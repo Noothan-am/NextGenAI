3274a3aca2a4812b2ce09b5ded1306cb50a18b1b
Noothan am <100135382+Noothan-am@users.noreply.github.com>
2024-01-13 22:41:27 +0530

    adds audiofile

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
-require("dotenv").config();
-require("./connection/connection");
 const express = require("express");
 const cors = require("cors");
-const authRouter = require("./routes/auth-router");
+const gTTS = require("gtts");
+const fs = require("fs");
+const path = require("path");
+const { exec } = require("child_process");
 
 const app = express();
 const port = process.env.PORT || 8000;
@@ -14,7 +15,49 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
-app.use(authRouter);
+
+app.get("/hello", (req, res) => {
+  let speech = "Welcome to GeeksforGeeks";
+  const gtts = new gTTS(speech, "en");
+
+  // Save the audio file in a temporary location
+  const audioFilePath = path.resolve(__dirname, "Voice.mp3");
+  gtts.save(audioFilePath, function (err, result) {
+    if (err) {
+      throw new Error(err);
+    }
+
+    const decodedVideoBuffer = Buffer.from(base64Video, "base64");
+
+    const videoFilePath = path.resolve(__dirname, "DecodedVideo.mp4");
+
+    fs.writeFileSync(videoFilePath, decodedVideoBuffer);
+
+    const combinedFilePath = path.resolve(__dirname, "CombinedVideo.mp4");
+    const ffmpegCommand = `ffmpeg -i ${videoFilePath} -i ${audioFilePath} -c:v copy -c:a aac -strict experimental -shortest ${combinedFilePath}`;
+
+    exec(ffmpegCommand, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error combining audio and video: ${stderr}`);
+        res.status(500).send("Internal Server Error");
+      } else {
+        // Send the combined video as a response
+        res.sendFile(combinedFilePath, {}, (err) => {
+          if (err) {
+            console.error("Error sending file: ", err);
+            res.status(err.status).end();
+          } else {
+            console.log("File sent successfully");
+            // Clean up temporary files if needed
+            fs.unlink(audioFilePath, () => {});
+            fs.unlink(videoFilePath, () => {});
+            fs.unlink(combinedFilePath, () => {});
+          }
+        });
+      }
+    });
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
